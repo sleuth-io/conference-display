144240091750ced7cf52ee2ea9b2ae082dbc7291
Don Brown <dbrown@sleuth.io>
2023-04-14 03:59:01 -0600

    New alt-tabing approach

--- src/sleuthconf/window.py
+++ src/sleuthconf/window.py
@@ -1,5 +1,11 @@
 from evdev import UInput, ecodes as e
-import pyautogui
+
 
 def alt_tab():
-    pyautogui.hotkey('alt', 'tab')
+    print("tabbing")
+    with UInput({e.EV_KEY : [e.KEY_LEFTALT, e.KEY_TAB]}) as ui:
+        ui.write(e.EV_KEY, e.KEY_LEFTALT, 1)
+        ui.write(e.EV_KEY, e.KEY_TAB, 1)
+        ui.write(e.EV_KEY, e.KEY_TAB, 0)
+        ui.write(e.EV_KEY, e.KEY_LEFTALT, 0)
+        ui.syn()
